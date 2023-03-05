c7895a90e6fd30c32aa83e5f1a9a5aceba46755c
marsk6 <17928921+marsk6@users.noreply.github.com>
2023-03-05 11:20:00 +0000

    Deploying to gh-pages from @ marsk6/marsk6.github.io@1451174b0ea6cc67bc6356c96a150a6b4c3479cb 🚀

--- _next/static/jlhGEYcStyWDT8cULAlWk/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Farchive","\u002Fpage\u002F[page]","\u002Fposts\u002F[slug]","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
