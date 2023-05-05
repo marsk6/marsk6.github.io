d7ec5cf98db25ed38c974d6f88c898211828cfcd
marsk <marsklee6@gmail.com>
2023-05-05 19:00:27 +0800

    feat: new button style

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,4 +1,4 @@
-import { cx } from '@emotion/css'
+import { css, cx } from '@emotion/css'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,14 +6,28 @@ type TagProps = {
   sup: string | number
   name: string
 }
+
+const shadow3D = css`
+  transition: 0.1 all;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2),
+    -4px -4px 8px rgba(255, 255, 255, 0.9), inset 0 0 0 rgba(255, 255, 255, 0.9),
+    inset 0 0 0 rgba(0, 0, 0, 0.4);
+  &:active {
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.2), 0 0 0 rgba(255, 255, 255, 0.9),
+      inset -4px -4px 8px rgba(255, 255, 255, 0.9),
+      inset 2px 2px 6px rgba(0, 0, 0, 0.4);
+  }
+`
+
 const Tag: React.FC<TagProps> = ({ sup, name }) => {
   return (
     <Link href={`/tags#${name}`} legacyBehavior passHref>
       <a
         className={cx(
-          'text-xs rounded-full py-1.5 px-3 font-medium ',
+          'text-xs rounded-full py-1.5 px-3 font-medium',
           'bg-stone-50 dark:bg-stone-900',
-          'hover:bg-stone-200, dark:hover:bg-stone-700'
+          'hover:bg-stone-200, dark:hover:bg-stone-400',
+          shadow3D
         )}
       >
         {name}
