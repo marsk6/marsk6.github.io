efffb5bc41a683bee03c2d38a695e3b90bee75b5
marsk <marsklee6@gmail.com>
2023-05-26 20:30:46 +0800

    chore: separate keystone

--- script/build.ts
+++ script/build.ts
@@ -1,4 +1,21 @@
 /**
  * 1. git 拉取 keystone admin 仓库并安装依赖
- * 
+ *
  */
+
+import { simpleGit } from 'simple-git'
+
+const main = async () => {
+  const git = simpleGit()
+  await git.clone()
+  await git.checkout('master')
+  await git.deleteLocalBranch('auto/release', true)
+  await git.checkoutBranch('auto/release', 'origin/master')
+  await cb()
+  await git.add('.')
+  await git.commit('release new articles')
+  await git.checkout('master')
+  await git.mergeFromTo('', 'auto/release')
+}
+
+main()
