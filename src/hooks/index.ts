6b9bd9c8bf9a6b2163d4879567c3bf8191ba0243
marsk <marsklee6@gmail.com>
2023-06-05 17:53:48 +0800

    feat: code block title

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const createLink = (href: string, id: string) => {
   const link = document.createElement('link')
@@ -25,7 +25,7 @@ const toggleCodeBlockTheme = (isDark: boolean) => {
         'dark-code-block-theme'
       )
     }
-    lightLink?.setAttribute('rel', '')
+    lightLink?.setAttribute('rel', 'disabled-stylesheet')
   } else {
     if (lightLink) {
       lightLink.rel = 'stylesheet'
@@ -35,25 +35,29 @@ const toggleCodeBlockTheme = (isDark: boolean) => {
         'light-code-block-theme'
       )
     }
-    darkLink?.setAttribute('rel', '')
+    darkLink?.setAttribute('rel', 'disabled-stylesheet')
   }
 }
 
 export const useDarkMode = () => {
   const [isDark, setDark] = useState(false)
+  const isInit = useRef(false)
   useEffect(() => {
-    const nextState = localStorage.getItem('themeMode') === 'dark'
-    setDark(nextState)
-  }, [])
-  useEffect(() => {
-    toggleCodeBlockTheme(isDark)
-    if (isDark) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('themeMode', 'dark')
+    if (!isInit.current) {
+      const nextState = localStorage.getItem('themeMode') === 'dark'
+      setDark(nextState)
+      isInit.current = true;
     } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('themeMode', 'light')
+      if (isDark) {
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('themeMode', 'dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('themeMode', 'light')
+      }
+      toggleCodeBlockTheme(isDark)
     }
+
   }, [isDark])
 
   return {
